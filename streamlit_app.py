86b4dac603ddd4bd2c3b73a5b30489cb8a957285
Harirak Intarak <meen_harirak@hotmail.com>
2024-05-14 08:21:28 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,27 @@
 import pandas as pd
 import streamlit as st
+import requests
+from io import StringIO
 # from sqlalchemy.orm import sessionmaker
 # from table_meta import mst_cat, seller_list
 
 st.set_page_config(layout="wide")
 # session = sessionmaker(bind=engine)()
 
+def load_original_data():
+    url = 'https://raw.githubusercontent.com/Harirak/test_streamlit/main/category_eda.csv?token=GHSAT0AAAAAACSH7IEFBSLIJH5SSIBFIKZ4ZSCXQYQ'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.read_csv(StringIO(response.text))
+    else:
+        st.error("Failed to load data from Github")
 
 # result = session.query(mst_cat.c.description).filter(mst_cat.c.level==5).all()
 # session.close()
 
 # opt = pd.DataFrame(result)
-df_opt = pd.read_excel("category_eda.xlsx")
-
+# df_opt = pd.read_excel("category_eda.xlsx")
+df_opt = load_original_data()
 
 opt_lv1 = df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].unique()
 options_lv1 = st.multiselect("What is Category LV4", list(opt_lv1))
