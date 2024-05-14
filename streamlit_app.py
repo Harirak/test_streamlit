645ac1c6a50ca1dadd5c957d0587fd0aacec7edd
Harirak Intarak <meen_harirak@hotmail.com>
2024-05-14 09:02:11 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,12 @@ df_opt = load_original_data()
 opt_lv1 = df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].unique()
 options_lv1 = st.multiselect("What is Category LV4", list(opt_lv1))
 
+st.divider()
+st.title("Select What is Product")
+df_opt['NEW SUBCATEGORY-L5 (463)'] = df_opt['NEW SUBCATEGORY-L5 (463)'].str.strip()
 
-opt = df_opt.loc[:, 'NEW SUBCATEGORY-L5 (463)'].to_list() if (options_lv1==[] or options_lv1==['All']) else df_opt.loc[df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].isin(options_lv1), 'NEW SUBCATEGORY-L5 (463)'].to_list()
+opt = df_opt.loc[:, 'NEW SUBCATEGORY-L5 (463)'].to_list() if (options_lv1==[] or options_lv1==['All']) else df_opt.loc[df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].isin(options_lv1), 'NEW SUBCATEGORY-L5 (463)'].str.strip().to_list()
+
 options_lv2 = st.multiselect("What is Product", opt, [])
 
 st.write("You selected:", options_lv2)
