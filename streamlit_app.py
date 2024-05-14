5cb6eee91b122d4943d573c74fb48ef537bf64b2
harirak.i <meen_harirak@hotmail.com>
2024-05-14 10:58:32 +0700

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import streamlit as st
 import requests
 from io import StringIO
 
+# 
+
 st.set_page_config(layout="wide")
 
 def load_original_data():
@@ -14,22 +16,30 @@ def load_original_data():
         st.error("Failed to load data from Github")
 
 df_opt = load_original_data()
-
-opt_lv1 = df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].unique()
-options_lv1 = st.multiselect("What is Category LV4", list(opt_lv1))
-
-st.divider()
-st.title("Select What is Product")
 df_opt['NEW SUBCATEGORY-L5 (463)'] = df_opt['NEW SUBCATEGORY-L5 (463)'].str.strip()
 
-opt = df_opt.loc[:, 'NEW SUBCATEGORY-L5 (463)'].to_list() if (options_lv1==[] or options_lv1==['All']) else df_opt.loc[df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].isin(options_lv1), 'NEW SUBCATEGORY-L5 (463)'].str.strip().to_list()
 
+
+############
+st.header("Select What is Product")
+
+opt = df_opt.loc[:, 'NEW SUBCATEGORY-L5 (463)'].to_list()  
 options_lv2 = st.multiselect("What is Product", opt, [])
 
 st.write("You selected:", options_lv2)
 
 st.divider()
+###########
 st.header("Search Remark")
 
 st.text_input("Remark: ", key='remark')
 st.table(df_opt.loc[df_opt['REMARK'].fillna('').str.contains(st.session_state.remark),['NEW SUBCATEGORY-L4 (88) - Sub Cate','NEW SUBCATEGORY-L5 (463)','REMARK']] if st.session_state.remark else None)
+
+st.divider()
+###########
+st.header("Check 400 Cat from 88 Cat")
+opt_lv1 = df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].unique()
+options_lv1 = st.multiselect("What is Category 88", list(opt_lv1))
+opt_ = df_opt.loc[df_opt['NEW SUBCATEGORY-L4 (88) - Sub Cate'].isin(options_lv1), ['NEW SUBCATEGORY-L4 (88) - Sub Cate','NEW SUBCATEGORY-L5 (463)','REMARK']]
+st.table(opt_)
+# options_lv2 = st.multiselect("What is Product", opt_, [])
